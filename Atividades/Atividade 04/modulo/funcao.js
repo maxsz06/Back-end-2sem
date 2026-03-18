73d502f35843234f844561b0e1a334b6a0778a4e
MAXWILLIAN ALBINO ZACARIAS DE SANTANA <25203648@CFP127ALUNO.LOCAL>
2026-03-18 11:30:31 -0300

    Requisição (getDadosEstado) - Concluida

--- Atividades/Atividade 04/modulo/funcao.js
+++ Atividades/Atividade 04/modulo/funcao.js
@@ -3,15 +3,23 @@ const infoEstados = require('./estados_cidades.js')
 
 function getListadeEstados(){
 
-    let qtdEstados = infoEstados.listaDeEstados.estados.length
+    let qtdEstados = Number(infoEstados.listaDeEstados.estados.length)
     let siglasUF = []
 
   for(let i = 0; i< qtdEstados; i++){
     siglasUF.push(infoEstados.listaDeEstados.estados[i].sigla)
   }
    console.log(siglasUF)
-   console.log (`quantidade: ${siglasUF.length}`)
+   console.log (`quantidade: ${qtdEstados}`)
 }
 
+
+function getDadosEstado(){
+
+let estadoName = 'Sao Paulo'
+
+ if(estadoName == infoEstados.listaDeEstados) 
+
+}
 
 getListadeEstados()
